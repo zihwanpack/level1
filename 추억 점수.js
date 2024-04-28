639b2ec373491c2b0ca10252fdfbbbb8f6e06cb1
Layton <jihwan8807@naver.com>
2024-04-29 02:17:18 +0900

    추억 점수

---
+++ 추억 점수.js
@@ -0,0 +1,17 @@
+function solution(name, yearning, photo) {
+  const answer = [];
+
+  photo.forEach((arr) => {
+    let sum = 0;
+    arr.forEach((e, i) => {
+      for (let j = 0; j < name.length; j++) {
+        if (name[j] === e) {
+          sum += yearning[j];
+        }
+      }
+    });
+    answer.push(sum);
+  });
+
+  return answer;
+}
