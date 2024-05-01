0dd3f140d045d6b1907e7a9d66a6decec3eec6d0
Layton <jihwan8807@naver.com>
2024-05-01 13:30:24 +0900

    소수 만들기

---
+++ 소수 만들기.js
@@ -0,0 +1,20 @@
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+function solution(nums) {
+  let result = 0;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) result++;
+      }
+    }
+  }
+  return result;
+}
