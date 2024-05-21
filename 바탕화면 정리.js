ce949255aa600c834c8bff6b29991bdd0f0c86f7
Layton <jihwan8807@naver.com>
2024-05-21 11:58:59 +0900

    바탕화면 정리

---
+++ 바탕화면 정리.js
@@ -0,0 +1,22 @@
+function solution(wallpaper) {
+  const answer = [];
+  let lux, luy, rdx, rdy;
+  const xArray = [];
+  const yArray = [];
+
+  wallpaper.forEach((line, x) => {
+    for (let y = 0; y < line.length; y++) {
+      if (line[y] === '#') {
+        xArray.push(x);
+        yArray.push(y);
+      }
+    }
+  });
+
+  answer.push(Math.min(...xArray));
+  answer.push(Math.min(...yArray));
+  answer.push(Math.max(...xArray) + 1);
+  answer.push(Math.max(...yArray) + 1);
+
+  return answer;
+}
