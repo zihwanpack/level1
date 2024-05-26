7160bc0639937d70fc59adc31b800c2c01142805
Layton <jihwan8807@naver.com>
2024-05-26 16:25:49 +0900

    달리기 경주

--- 달리기 경주.js
+++ 달리기 경주.js
@@ -1,15 +1,20 @@
 function solution(players, callings) {
-  // 처음 배열로 접근했으나 O(n^2)라 시간초과함
-  // callings.forEach((calling) => {
-  //   players.forEach((player, index) => {
-  //     if (calling === player && index > 0) {
-  //       [
-  //         players[index - 1],
-  //         (players[index] = players[index]),
-  //         players[index - 1],
-  //       ];
-  //     }
-  //   });
-  // });
-  // return players;
+  const playerObj = {};
+
+  for (let i = 0; i < players.length; i++) {
+    const currentPlayer = players[i];
+    playerObj[currentPlayer] = i;
+  }
+
+  for (let j = 0; j < callings.length; j++) {
+    const currentPlayer = callings[j];
+    const index = playerObj[currentPlayer];
+
+    [players[index - 1], players[index]] = [players[index], players[index - 1]];
+
+    playerObj[currentPlayer] = index - 1;
+    playerObj[players[index]] = index;
+  }
+
+  return players;
 }
